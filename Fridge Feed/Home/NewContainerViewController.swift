59637a3dd7c3af3e3b35e382a17356b2c24779af
Maxime Moison <moisonmaxime@gmail.com>
2018-12-02 15:26:43 -0800

    progress (more)

---
+++ Fridge Feed/Home/NewContainerViewController.swift
@@ -0,0 +1,54 @@
+//
+//  NewContainerViewController.swift
+//  Fridge Feed
+//
+//  Created by Maxime Moison on 12/2/18.
+//  Copyright © 2018 Maxime Moison. All rights reserved.
+//
+
+import UIKit
+
+class NewContainerViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var typeTextField: UITextField!
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @IBAction func saveTap() {
+        guard let name = nameTextField.text else {
+            displayAlert(title: "Missing", message: "Enter name")
+            return
+        }
+        guard let type = typeTextField.text else {
+            displayAlert(title: "Missing", message: "Enter type")
+            return
+        }
+        
+        RestAPI.createContainer(name: name,
+                                type: type,
+                                completionHandler: {
+                                    self.dismiss(animated: true)
+        }, errorHandler: handleError)
+    }
+    
+    @IBAction func cancelTap() {
+        self.dismiss(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
